1d24cc719c6812fd7b7c29daac94db4ac99cddb3
Alfred Isair <163370258+Alfred9@users.noreply.github.com>
2024-04-25 10:28:44 +0300

    Add files via upload

---
+++ Semantic-search/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from transformers import AutoModel
+from numpy.linalg import norm
+
+@st.cache_resource
+def load_model():
+    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True)
+    return model
+
+cos_sim = lambda a, b: (a @ b.T) / (norm(a) * norm(b))
+model = load_model()
+
+def compare_sentences(input_sentences, sentences):
+    output = []
+    for input_sentence in input_sentences:
+        input_embedding = model.encode([input_sentence])[0]
+        sentence_results = []
+        for sentence in sentences:
+            sentence_embedding = model.encode([sentence])[0]
+            similarity = cos_sim(input_embedding, sentence_embedding)
+            sentence_results.append((sentence, similarity))
+        output.append((input_sentence, sentence_results))
+    return output
+
+def main():
+    st.title("Sentence Similarity ")
+
+    input_sentences = st.text_area("Input Sentence", height=100).split("\n")
+
+    sentences = st.text_area("Sentence to Compare", height=200).split("\n")
+
+    if input_sentences and sentences:
+        results = compare_sentences(input_sentences, sentences)
+        st.subheader("Results")
+        for input_sentence, sentence_results in results:
+            st.write(f"Input Sentence: {input_sentence}")
+            for sentence, similarity in sentence_results:
+                st.write(f"{sentence} - Similarity: {similarity:.2f}")
+            st.write("---")
+
+if __name__ == "__main__":
+    main()
