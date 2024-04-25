abad442c6e98a0cd69faf64a8c9cdd16b0e402cd
Alfred Isair <163370258+Alfred9@users.noreply.github.com>
2024-04-25 11:52:19 +0300

    Update app.py

--- Semantic-search/app.py
+++ Semantic-search/app.py
@@ -4,7 +4,7 @@ from numpy.linalg import norm
 
 @st.cache_resource
 def load_model():
-    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True, revision="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")
+    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True, revision="0f472a4cde0e6e50067b8259a3a74d1110f4f8d8")
     return model
 
 cos_sim = lambda a, b: (a @ b.T) / (norm(a) * norm(b))
